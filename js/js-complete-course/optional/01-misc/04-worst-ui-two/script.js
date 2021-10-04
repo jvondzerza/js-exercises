db18aae311cef746c7eca2dff0117dee971e3de8
jvondzerza <jvondzerza@googlemail.com>
2021-10-04 13:55:14 +0200

    completed up until optional 01-06

--- js/js-complete-course/optional/01-misc/04-worst-ui-two/script.js
+++ js/js-complete-course/optional/01-misc/04-worst-ui-two/script.js
@@ -12,5 +12,30 @@
 (function() {
 
     // your code here
+    let target = document.getElementById("target");
+    let b1 = document.getElementById("part-one");
+    let b2 = document.getElementById("part-two");
+    let b3 = document.getElementById("part-three");
+    let b4 = document.getElementById("part-four");
+    let buttons = [b1, b2, b3, b4];
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            let min = button.getAttribute("data-min")
+            let max = button.getAttribute("data-max")
+            let result = parseInt(button.innerHTML) + 1;
+            if (result > max) {
+                result = min
+            }
+            if (result < 10) {
+                button.innerHTML = "0" + result;
+            } else {
+                button.innerHTML = result.toString();
+            }
+            target.innerText = "+" + b1.innerText + b2.innerText + b3.innerText + b4.innerText;
+
+        })
+    })
+
 
 })();
--- js/js-complete-course/optional/01-misc/06-guess-number/script.js
+++ js/js-complete-course/optional/01-misc/06-guess-number/script.js
@@ -12,5 +12,16 @@
 (function() {
 
     // your code here
-
+    let x = Math.floor(Math.random() * 100) + 1;
+    let guess = window.prompt("Guess the number between 1 and 100.");
+    for (let i = 1; i < guess.length; i++) {
+        if (guess < x) {
+            guess = window.prompt("Higher!")
+        } else if (guess > x) {
+            guess = window.prompt("Lower")
+        } else if (guess = x) {
+            window.prompt("Spot on, good guess!")
+        }
+    }
+    if (i = 1)
 })();
